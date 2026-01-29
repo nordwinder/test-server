77855d3dcf4eee8a4f9017dd849b9c34cf4ba386
nordwinder <therandomchoice@proton.me>
2026-01-29 14:06:26 +0400

    another value added here

--- src/api.rs
+++ src/api.rs
@@ -5,6 +5,7 @@ use utoipa::ToSchema;
 #[derive(Serialize, Deserialize, ToSchema, Default)]
 pub struct HelloResponse {
     pub message: String,
+    pub qqq: i32,
     //pub number: i32,
     //pub value: String,
     //pub some: bool,
