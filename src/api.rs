6c18fba859b5d3b305ad1ecf06298934f287921b
nordwinder <therandomchoice@proton.me>
2026-01-29 13:58:17 +0400

    ttt

--- src/api.rs
+++ src/api.rs
@@ -5,9 +5,9 @@ use utoipa::ToSchema;
 #[derive(Serialize, Deserialize, ToSchema, Default)]
 pub struct HelloResponse {
     pub message: String,
-    pub number: i32,
-    pub value: String,
-    pub some: bool,
+    //pub number: i32,
+    //pub value: String,
+    //pub some: bool,
 }
 //
 #[utoipa::path(
